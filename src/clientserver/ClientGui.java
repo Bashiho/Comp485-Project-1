929f358322649e71b9c0de8f51f2d6d35350ad9d
Bashiho <basilrdv74@gmail.com>
2026-04-06 23:54:46 -0700

    updates to some UI features

--- src/clientserver/ClientGui.java
+++ src/clientserver/ClientGui.java
@@ -1,3 +1,6 @@
+package clientserver;
+
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,7 +21,6 @@ import java.util.Arrays;
 public class ClientGui extends Thread{
 
   final JTextPane jtextFilDiscu = new JTextPane();
-  final JTextPane jtextListUsers = new JTextPane();
   final JTextField jtextInputChat = new JTextField();
   private String oldMsg = "";
   private Thread read;
@@ -33,8 +35,8 @@ public class ClientGui extends Thread{
   public ClientGui() {
     this.serverName = "localhost";
     this.PORT = 1234;
-    this.name = "username";
-    this.password = "password";
+    this.name = "Username";
+    this.password = "Password";
 
     String fontfamily = "Arial, sans-serif";
     Font font = new Font(fontfamily, Font.PLAIN, 15);
@@ -46,27 +48,15 @@ public class ClientGui extends Thread{
     jfr.setResizable(false);
     jfr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    jtextFilDiscu.setBounds(25, 25, 490, 320);
+    jtextFilDiscu.setBounds(25, 25, 650, 320);
     jtextFilDiscu.setFont(font);
     jtextFilDiscu.setMargin(new Insets(6, 6, 6, 6));
     jtextFilDiscu.setEditable(false);
     JScrollPane jtextFilDiscuSP = new JScrollPane(jtextFilDiscu);
-    jtextFilDiscuSP.setBounds(25, 25, 490, 320);
+    jtextFilDiscuSP.setBounds(25, 25, 650, 320);
 
     jtextFilDiscu.setContentType("text/html");
     jtextFilDiscu.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
-
-    // Box for user list, not properly implemented yet
-    jtextListUsers.setBounds(520, 25, 156, 320);
-    jtextListUsers.setEditable(true);
-    jtextListUsers.setFont(font);
-    jtextListUsers.setMargin(new Insets(6, 6, 6, 6));
-    jtextListUsers.setEditable(false);
-    JScrollPane jsplistuser = new JScrollPane(jtextListUsers);
-    jsplistuser.setBounds(520, 25, 156, 320);
-
-    jtextListUsers.setContentType("text/html");
-    jtextListUsers.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
 
     // Box for user input
     jtextInputChat.setBounds(0, 350, 400, 50);
@@ -76,13 +66,13 @@ public class ClientGui extends Thread{
     jtextInputChatSP.setBounds(25, 350, 650, 50);
 
     // Buttons for sending messages and disconnecting from server
-    final JButton jsbtn = new JButton("Send");
-    jsbtn.setFont(font);
-    jsbtn.setBounds(575, 410, 100, 35);
+    final JButton sendMessage = new JButton("Send Message");
+    sendMessage.setFont(font);
+    sendMessage.setBounds(475, 410, 200, 35);
 
-    final JButton jsbtndeco = new JButton("Disconnect");
-    jsbtndeco.setFont(font);
-    jsbtndeco.setBounds(25, 410, 130, 35);
+    final JButton disconnectButton = new JButton("Disconnect");
+    disconnectButton.setFont(font);
+    disconnectButton.setBounds(25, 410, 130, 35);
 
     jtextInputChat.addKeyListener(new KeyAdapter() {
       public void keyPressed(KeyEvent e) {
@@ -104,58 +94,56 @@ public class ClientGui extends Thread{
       }
     });
 
-    jsbtn.addActionListener(new ActionListener() {
+    sendMessage.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent ae) {
         sendMessage();
       }
     });
 
     // Initial text for input fields
-    final JTextField jtfPass = new JTextField(this.password);
-    final JTextField jtfName = new JTextField(this.name);
-    final JTextField jtfport = new JTextField(Integer.toString(this.PORT));
-    final JTextField jtfAddr = new JTextField(this.serverName);
-    final JButton jcbtn = new JButton("Connect");
+    final JTextField passwordInput = new JTextField(this.password);
+    final JTextField usernameInput = new JTextField(this.name);
+    final JTextField portInput = new JTextField(Integer.toString(this.PORT));
+    final JTextField addressInput = new JTextField(this.serverName);
+    final JButton connectButton = new JButton("Connect");
 
-    jtfPass.getDocument().addDocumentListener(new TextListener(jtfPass, jtfName, jtfport, jtfAddr, jcbtn));
-    jtfName.getDocument().addDocumentListener(new TextListener(jtfPass, jtfName, jtfport, jtfAddr, jcbtn));
-    jtfport.getDocument().addDocumentListener(new TextListener(jtfPass, jtfName, jtfport, jtfAddr, jcbtn));
-    jtfAddr.getDocument().addDocumentListener(new TextListener(jtfPass, jtfName, jtfport, jtfAddr, jcbtn));
+    passwordInput.getDocument().addDocumentListener(new TextListener(passwordInput, usernameInput, portInput, addressInput, connectButton));
+    usernameInput.getDocument().addDocumentListener(new TextListener(passwordInput, usernameInput, portInput, addressInput, connectButton));
+    portInput.getDocument().addDocumentListener(new TextListener(passwordInput, usernameInput, portInput, addressInput, connectButton));
+    addressInput.getDocument().addDocumentListener(new TextListener(passwordInput, usernameInput, portInput, addressInput, connectButton));
 
     // Sets position and size of each field and button for signin process
-    jcbtn.setFont(font);
-    jtfAddr.setBounds(25, 380, 135, 40);
-    jtfName.setBounds(375, 380, 135, 40);
-    jtfport.setBounds(200, 380, 135, 40);
-    jtfPass.setBounds(550, 380, 135, 40); 
-    jcbtn.setBounds(575, 425, 100, 40);
+    connectButton.setFont(font);
+    addressInput.setBounds(25, 380, 135, 40);
+    usernameInput.setBounds(375, 380, 135, 40);
+    portInput.setBounds(200, 380, 135, 40);
+    passwordInput.setBounds(550, 380, 135, 40); 
+    connectButton.setBounds(575, 425, 100, 40);
 
     jtextFilDiscu.setBackground(Color.LIGHT_GRAY);
-    jtextListUsers.setBackground(Color.LIGHT_GRAY);
 
-    jfr.add(jcbtn);
+    jfr.add(connectButton);
     jfr.add(jtextFilDiscuSP);
-    jfr.add(jsplistuser);
-    jfr.add(jtfPass);
-    jfr.add(jtfName);
-    jfr.add(jtfport);
-    jfr.add(jtfAddr);
+    jfr.add(passwordInput);
+    jfr.add(usernameInput);
+    jfr.add(portInput);
+    jfr.add(addressInput);
     jfr.setVisible(true);
 
     // When user signs in, prints chat commands in chat
     appendToPane(jtextFilDiscu, "<h4>Chat Commands:</h4>"
         +"<ul>"
-        +"<li><b>@nickname</b> send a private message to user</li>"
-        +"<li><b>#d3961b</b> change color of username</li>"
+        +"<li>To private message user, use: <b>@nickname</b> </li>"
+        +"<li>To change username color, use: <b>#d3961b</b> </li>"
         +"</ul><br/>");
 
-    jcbtn.addActionListener(new ActionListener() {
+    connectButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent ae) {
         try {
-          password = jtfPass.getText();
-          name = jtfName.getText();
-          String port = jtfport.getText();
-          serverName = jtfAddr.getText();
+          password = passwordInput.getText();
+          name = usernameInput.getText();
+          String port = portInput.getText();
+          serverName = addressInput.getText();
           PORT = Integer.parseInt(port);
 
           appendToPane(jtextFilDiscu, "<span>Connecting to " + serverName + " on port " + PORT + "...</span>");
@@ -174,18 +162,17 @@ public class ClientGui extends Thread{
           // Changes buttons and fields when user signs in
           read = new Read();
           read.start();
-          jfr.remove(jtfPass);
-          jfr.remove(jtfName);
-          jfr.remove(jtfport);
-          jfr.remove(jtfAddr);
-          jfr.remove(jcbtn);
-          jfr.add(jsbtn);
+          jfr.remove(passwordInput);
+          jfr.remove(usernameInput);
+          jfr.remove(portInput);
+          jfr.remove(addressInput);
+          jfr.remove(connectButton);
+          jfr.add(sendMessage);
           jfr.add(jtextInputChatSP);
-          jfr.add(jsbtndeco);
+          jfr.add(disconnectButton);
           jfr.revalidate();
           jfr.repaint();
           jtextFilDiscu.setBackground(Color.WHITE);
-          jtextListUsers.setBackground(Color.WHITE);
         } catch (Exception ex) {
           appendToPane(jtextFilDiscu, "<span>Could not connect to Server</span>");
           JOptionPane.showMessageDialog(jfr, ex.getMessage());
@@ -194,23 +181,21 @@ public class ClientGui extends Thread{
 
     });
 
-    jsbtndeco.addActionListener(new ActionListener()  {
+    disconnectButton.addActionListener(new ActionListener()  {
       public void actionPerformed(ActionEvent ae) {
         // When user disconnects, switch back to appropriate input fields
-        jfr.add(jtfPass);
-        jfr.add(jtfName);
-        jfr.add(jtfport);
-        jfr.add(jtfAddr);
-        jfr.add(jcbtn);
-        jfr.remove(jsbtn);
+        jfr.add(passwordInput);
+        jfr.add(usernameInput);
+        jfr.add(portInput);
+        jfr.add(addressInput);
+        jfr.add(connectButton);
+        jfr.remove(sendMessage);
         jfr.remove(jtextInputChatSP);
-        jfr.remove(jsbtndeco);
+        jfr.remove(disconnectButton);
         jfr.revalidate();
         jfr.repaint();
         read.interrupt();
-        jtextListUsers.setText(null);
         jtextFilDiscu.setBackground(Color.LIGHT_GRAY);
-        jtextListUsers.setBackground(Color.LIGHT_GRAY);
         appendToPane(jtextFilDiscu, "<span>Connection closed.</span>");
         output.close();
       }
@@ -282,7 +267,6 @@ public class ClientGui extends Thread{
     ClientGui client = new ClientGui();
   }
   
-  // Handles user list, not working
   class Read extends Thread {
     public void run() {
       String message;
@@ -292,13 +276,6 @@ public class ClientGui extends Thread{
           if(message != null){
             if (message.charAt(0) == '[') {
               message = message.substring(1, message.length()-1);
-              ArrayList<String> ListUser = new ArrayList<String>(
-                  Arrays.asList(message.split(", "))
-                  );
-              jtextListUsers.setText(null);
-              for (String user : ListUser) {
-                appendToPane(jtextListUsers, "@" + user);
-              }
             }else{
               appendToPane(jtextFilDiscu, message);
             }
@@ -310,7 +287,7 @@ public class ClientGui extends Thread{
       }
     }
   }
-
+  
   private void appendToPane(JTextPane tp, String msg){
     HTMLDocument doc = (HTMLDocument)tp.getDocument();
     HTMLEditorKit editorKit = (HTMLEditorKit)tp.getEditorKit();
